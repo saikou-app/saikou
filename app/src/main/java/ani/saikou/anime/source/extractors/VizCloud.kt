c6ef5dfd8d131f32c0c0db4cf8396717542eeb75
Sutslec <me@comor.bid>
2022-04-20 20:58:24 +0200

    WIP adding 9anime

---
+++ app/src/main/java/ani/saikou/anime/source/extractors/VizCloud.kt
@@ -0,0 +1,11 @@
+package ani.saikou.anime.source.extractors
+
+import ani.saikou.anime.Episode
+import ani.saikou.anime.source.Extractor
+
+class VizCloud: Extractor(){
+    override fun getStreamLinks(name: String, url: String): Episode.StreamLinks {
+        TODO("Not yet implemented")
+    }
+
+}
