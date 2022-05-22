6c6f447777dbb5dcfe47034071c230377636d68b
Diego Peña Y Lillo <80992641+Diegopyl1209@users.noreply.github.com>
2022-05-22 01:09:19 -0400

    more servers [Hentaimama]

--- app/src/main/java/ani/saikou/parsers/anime/Hentaimama.kt
+++ app/src/main/java/ani/saikou/parsers/anime/Hentaimama.kt
@@ -1,8 +1,9 @@
 package ani.saikou.parsers.anime
 
+
 import ani.saikou.client
-import ani.saikou.getSize
 import ani.saikou.parsers.*
+import org.json.JSONArray
 
 class Hentaimama : AnimeParser() {
     override val name = "Hentaimama"
@@ -21,21 +22,33 @@ class Hentaimama : AnimeParser() {
         }
     }
 
-    override suspend fun loadVideoServers(episodeLink: String, extra: Any?): List<VideoServer> = mutableListOf(VideoServer("jwp", embedUrl = episodeLink))
+    override suspend fun loadVideoServers(episodeLink: String, extra: Any?): List<VideoServer>  {
+        val servers = mutableListOf<VideoServer>()
+        val animeId = client.get(episodeLink).document.select("#post_report > input:nth-child(5)").attr("value")
+        val body = client.post("https://hentaimama.io/wp-admin/admin-ajax.php", data = mapOf(
+            "action" to "get_player_contents",
+            "a" to animeId
+        ))
+        val json = JSONArray(body.toString())
+        for (i in 0 until json.length()){
+            val url = json[i].toString().substringAfter("src=\"").substringBefore("\"")
+            servers.add(VideoServer("Server $i",url))
+        }
+
+        return servers
+    }
 
     override suspend fun getVideoExtractor(server: VideoServer): VideoExtractor = HentaimamaExtractor(server)
 
     class HentaimamaExtractor(override val server: VideoServer) : VideoExtractor() {
         override suspend fun extract(): VideoContainer {
-            val animeId = client.get(server.embed.url).document.select("#post_report > input:nth-child(5)").attr("value")
-            val body = client.post("https://hentaimama.io/wp-admin/admin-ajax.php", data = mapOf(
-                "action" to "get_player_contents",
-                "a" to animeId
-            )).document
-            val embedUrl = body.toString().substringAfter("src=\"\\&quot;").substringBefore("\\&")
-            val url = client.get(embedUrl).document.select("script").toString().substringAfter("file: \"").substringBefore("\"")
+            var url = "https://hentaimama.io" + client.get(server.embed.url).document.select("script").toString()
+                .substringAfter("open('").substringBefore("');")
+            if(url.contains("<script type=\"text/javascript\">")){
+                url = "https://hentaimama.io" + url.substringAfter("downloadURL: '").substringBefore("'")
+            }
+            return VideoContainer(listOf(Video(null,false,url)))
 
-            return VideoContainer(listOf(Video(720,false,url, getSize(url))))
         }
     }
 
