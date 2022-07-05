343774cad548c40332ee0d02d3043f49aef715fa
Adolar0042 <39769465+Adolar0042@users.noreply.github.com>
2022-07-05 09:46:30 +0200

    Maybe this fixes it?

--- app/src/main/java/ani/saikou/anime/SelectorDialogFragment.kt
+++ app/src/main/java/ani/saikou/anime/SelectorDialogFragment.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.net.Uri
 import androidx.core.view.updateLayoutParams
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
